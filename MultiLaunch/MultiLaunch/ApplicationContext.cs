ca1b3dbbf7d098dd0565e02d0dac30598a55da79
Mikhail <lyttma163@gmail.com>
2024-05-17 20:09:09 +0300

    db connection + preset list from databse

---
+++ MultiLaunch/MultiLaunch/ApplicationContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace MultiLaunch
+{
+    class ApplicationContext : DbContext
+    {
+        public DbSet<Program> Program { get; set; }
+
+        public ApplicationContext() : base("DefaultConnection") { }
+    }
+}
